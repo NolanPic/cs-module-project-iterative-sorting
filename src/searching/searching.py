f4cedf17cf4667f299ed9ff5214764ddbe88cbf1
Nolan Picini <nolanpic@gmail.com>
2020-05-18 13:37:51 -0700

    complete searching.py (w/ time/space complexities)

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,25 @@
+# time: O(n)
+# space: O(1)
 def linear_search(arr, target):
-    # Your code here
-
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
+# time: O(log n)
+# space: O(1)
 def binary_search(arr, target):
-
-    # Your code here
-
+    left = 0
+    right = len(arr)-1
+    while left <= right:
+        mid = (left+right) // 2
+        if arr[mid] == target:
+            return mid
+        if arr[mid] < target:
+            left = mid+1
+        if arr[mid] > target:
+            right = mid-1
 
     return -1  # not found
